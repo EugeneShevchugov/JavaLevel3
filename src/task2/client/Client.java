75d77627218c994fad22a868a76a4abe3aaa9826
@eugene.shevchugov <eugene.shevchugov@gmail.com>
2020-08-24 17:19:39 +0600

    Third Task

---
+++ src/task2/client/Client.java
@@ -0,0 +1,110 @@
+package task2.client;
+
+import task2.client.GUI.LoginChatFrame;
+import task2.client.GUI.MainChatFrame;
+import task2.server.Server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client implements AuthorizationChecker{
+    private int port;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private MainChatFrame chatFrame;
+
+
+
+    public Client(int port) {
+        this.port = port;
+    }
+
+    /**
+     * Инициализация подключений, вызов диалогового окна с логином и общего окна
+     */
+    private void init(String login, String password) throws IOException {
+        //Коннекты к серверу
+        socket = new Socket("localhost", port);
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
+
+        //Диалоговое окно с логином и паролем. В качестве метода авторизации
+        //используется перегруженный метод checkAuthorization из Client
+        LoginChatFrame loginFrame = new LoginChatFrame(this, login, password);
+        System.out.println(loginFrame.isAuthorized());
+        loginFrame.dispose();
+
+        //Основное окно чата
+        chatFrame = new MainChatFrame("Клиент чата " + login,
+                new MessageListener() {
+                    @Override
+                    public void messagePerformed(String message) {
+                        try {
+                            out.writeUTF(message);
+                        } catch (IOException ioException) {
+                            throw new RuntimeException("Sending message error", ioException);
+                        }
+                    }
+                });
+    }
+
+    /**
+     * Запуск клиента. Логин и пароль передаются для удобства отладки, ну или если мы их сохраним в реестре
+     */
+    public void start(String login, String password) throws IOException {
+
+        init(login, password);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+
+                    while (true) {
+                        String message = in.readUTF();
+                        if (message.equals(Server.END_MESSAGE)) {
+                            System.out.println("Session closed. Cau!");
+                            break;
+                        }
+
+                        if (message.startsWith(Server.PRIVATE_MESSAGE)){
+                            chatFrame.prepareMessage(Server.WHISP_MESSAGE + message.split(":")[0].split("\\s")[Server.PRIVATE_MESSAGE.split("\\s").length] + " ");
+                        }
+
+                        chatFrame.pushMessage(message);
+
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        thread.start();
+    }
+
+    /**
+     * Метод проверки авторизации из интерфейса AutorizationChecker
+     * Получает логин и пароль, соединяется с сервером и проверяет, существует ли
+     * пользователь с указанным логином и паролем.
+     *
+     */
+    @Override
+    public boolean checkAuthorization(String login, String password) {
+        try {
+            out.writeUTF(Server.AUTH_MESSAGE + " " + login + " " + password);
+            String message = in.readUTF();
+            if (message.startsWith(Server.AUTH_DONE_MESSAGE)) {
+                System.out.println("Authorized");
+                return true;
+            }
+        }
+        catch (IOException exception){
+            throw new RuntimeException("Ошибка авторизации", exception);
+        }
+        return false;
+    }
+}
---
+++ src/task2/server/Server.java
@@ -0,0 +1,184 @@
+package task2.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.*;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Server {
+    static final String JDBC_DRIVER = "org.postgresql.Driver";
+    static final String DB_URL = "jdbc:postgresql://127.0.0.1:5432/chat";
+    static final String DB_USER = "root";
+    static final String DB_PASS = "";
+
+    public static final int PORT = 8082;
+
+    public static final String AUTH_MESSAGE = "/auth";
+    public static final String AUTH_DONE_MESSAGE = "/authok";
+    public static final String WHISP_MESSAGE = "/w ";
+    public static final String END_MESSAGE = "/end";
+    public static final String PRIVATE_MESSAGE = "Private message from";
+
+    private AuthService authService;
+    private Set<ClientHandler> clientHandlers;
+
+    private Connection dataBaseConnection = null;
+
+    public Server() {
+        this(PORT);
+    }
+
+    public Server(int port) {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+
+            initSQLServer();
+            initDatabase();
+
+            authService = new DataBaseAuthService(dataBaseConnection);
+            System.out.println("Auth is started up");
+
+            clientHandlers = new HashSet<>();
+
+            while (true) {
+                System.out.println("Waiting for a connection...");
+                Socket socket = serverSocket.accept();
+                System.out.println("Client connected: " + socket);
+                new ClientHandler(this, socket);
+            }
+        } catch (ClassNotFoundException | SQLException | IOException exception) {
+            //Все эксепшены в кучу, обрабатывать я их всё равно пока не умею
+            exception.printStackTrace();
+        } finally {
+            //Закрываем коннект
+            try {
+                if (dataBaseConnection != null) dataBaseConnection.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+    }
+
+
+    public AuthService getAuthService() {
+        return authService;
+    }
+
+    public synchronized boolean isOccupied(AuthService.Record record) {
+        for (ClientHandler ch : clientHandlers) {
+            if (ch.getRecord().equals(record)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public synchronized void subscribe(ClientHandler ch) {
+        clientHandlers.add(ch);
+    }
+
+    public synchronized void unsubscribe(ClientHandler ch) {
+        clientHandlers.remove(ch);
+    }
+
+    public synchronized void sendMessage(String name, String message) {
+
+        if (message.toLowerCase().startsWith(WHISP_MESSAGE)) {
+            sendPrivateMessage(name, message);
+        } else {
+            broadcastMessage(name, message);
+        }
+    }
+
+    private void broadcastMessage(String name, String message) {
+        for (ClientHandler ch : clientHandlers) {
+            ch.sendMessage(String.format("%s: %s", name, message));
+        }
+    }
+
+    private void sendPrivateMessage(String name, String message) {
+        ClientHandler sender = getClientHandlerByName(name);
+
+        String[] split = message.split("\\s");
+
+        if (split.length == 1) {
+            sender.sendMessage(String.format("Whisp command format: %sname message", Server.WHISP_MESSAGE));
+            return;
+        }
+
+        String recipientName = split.length > 1 ? split[1] : "";
+        String privateMessage = message.substring(split[0].length() + split[1].length() + 1);
+
+        if (privateMessage.isBlank()) {
+            sender.sendMessage("You should type message before sending");
+            return;
+        }
+
+        ClientHandler recipient = getClientHandlerByName(recipientName);
+        if (recipient == null) {
+            sender.sendMessage(String.format("Unknown username: %s", recipientName));
+            return;
+        }
+
+        if (name.equalsIgnoreCase(recipientName)) {
+            sender.sendMessage("You sent message to yourself: " + privateMessage);
+            return;
+        }
+
+        sender.sendMessage(String.format("You sent private message to %s: %s", recipientName, privateMessage));
+        recipient.sendMessage(String.format("%s %s: %s", PRIVATE_MESSAGE, name, privateMessage));
+
+    }
+
+    private ClientHandler getClientHandlerByName(String name) {
+        for (ClientHandler ch : clientHandlers) {
+            if (ch.getRecord().getName().toLowerCase().equals(name.toLowerCase())) {
+                return ch;
+            }
+        }
+        return null;
+    }
+
+    public void initSQLServer() throws SQLException, ClassNotFoundException {
+        Class.forName(Server.JDBC_DRIVER);
+        dataBaseConnection = DriverManager.getConnection(Server.DB_URL, Server.DB_USER, Server.DB_PASS);
+    }
+
+    /**
+     * Создаем таблицы в базе данных и наполняем юзерами, если их не было
+     */
+    public void initDatabase() throws SQLException {
+
+        try (Statement statement = dataBaseConnection.createStatement()) {
+            //language=SQL
+            String sql = "CREATE TABLE IF NOT EXISTS CHAT_USERS" +
+                    "(id INTEGER IDENTITY," +
+                    "name VARCHAR(255)," +
+                    "login VARCHAR(255)," +
+                    "password VARCHAR(255)," +
+                    "PRIMARY KEY ( id ))";
+            statement.executeUpdate(sql);
+
+            sql = "SELECT COUNT(*) as cnt FROM CHAT_USERS";
+            statement.execute(sql);
+
+            ResultSet resultSet = statement.getResultSet();
+            if (!resultSet.first()) {
+                System.out.println("Ошибка получения количества записей в CHAT_USERS!");
+                return;
+            }
+            if (resultSet.getInt("cnt") == 0) {
+
+                sql = "INSERT INTO CHAT_USERS(name, login, password)" +
+                        "VALUES('Barboss', 'l1', 'p1')" +
+                        ",('Kelvin', 'l2', 'p2')" +
+                        ",('Nicky', 'l3', 'p3')" +
+                        ",('Klaus', 'l4', 'p4')";
+
+                statement.executeUpdate(sql);
+            }
+
+        }
+    }
+}
